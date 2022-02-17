2b693502c47d5f78a3f3dface6abe83e3a25afe0
Emil Dzhebrailov <emil.dzhebrailov@yahoo.com>
2022-02-17 10:57:33 -0500

    framed-out-api

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const {getTasks} = require('./src/task');
+const PORT = process.nextTick.PORT || 3000;
+
+
+const app = express() 
+app.use(cors());
+app.use(express.json());
+
+//Routes
+//app.post('/tasks',createTask);
+app.get('/tasks',getTasks);
+//app.patch('/tasks/:taskId',updateTask);
+
+
+
+app.listen(PORT, () => {
+    console.log('Listening on Port: ', PORT)
+});
