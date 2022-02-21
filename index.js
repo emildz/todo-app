0f06f7dc8ef24d203f0352888a41e9d10ba1ffb2
Emil Dzhebrailov <emil.dzhebrailov@yahoo.com>
2022-02-21 10:23:15 -0500

    updated and fix the error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const {getTasks, createTask, updateTask} = require('./src/task');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 
 const app = express() 
--- src/task.js
+++ src/task.js
@@ -1,10 +1,10 @@
 const { connectDb } = require("./connectDb");
 
 exports.createTask = (request, response) => {
-  // const newTask = {
-  //     task: request.body.task,
-  //     done: false
-  // }
+  const newTask = {
+      task: request.body.task,
+      done: false
+  }
   const db = connectDb();
   db.collection("tasks")
     .add(newTask)
@@ -17,15 +17,15 @@ exports.getTasks = (request, response) => {
   db.collection("tasks")
     .get()
     .then((snapshot) => {
-      const taskList = snapshot.docs.map((doc) => {
-        let task = doc.date();
+      const taskList =  snapshot.docs.map((doc) => {
+        let task = doc.data();
         task.id = doc.id;
         return task;
       });
       response.send(taskList);
     })
     .catch((err) => response.status(500).send(err));
-};
+}
 exports.updateTask = (request, response) => {
   const { taskId } = request.params;
   const isDone = request.body.done;
